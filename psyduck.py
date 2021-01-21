5e76abfce05064230e621e6637529a75269cf9be
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2021-01-21 20:19:20 +0100

    Update psyduck.py

--- psyduck.py
+++ psyduck.py
@@ -185,7 +185,7 @@ async def on_message(message):
             if has_key(emote, i):
                 text += emote[i]
             else:
-                text += "‏‏‎　"
+                text += "‏‏‎　  "
             if (i + 1) % 6 == 0:
                 text += "│\n"
             else:
