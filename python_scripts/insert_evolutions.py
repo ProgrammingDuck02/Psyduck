217b5ce99a22ee13ac75ab65006b3e02c18b4e6f
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2021-01-24 20:02:59 +0100

    Update insert_evolutions.py

--- python_scripts/insert_evolutions.py
+++ python_scripts/insert_evolutions.py
@@ -47,6 +47,22 @@ def insert(table, fields, values):
     DB.commit()
     return True
 
+def update(table, fields, values, conditions):
+    connect_db()
+    global cursor, DB
+    if len(fields) == 0:
+        return False
+    if len(fields) != len(values):
+        return False
+    sql = "UPDATE "+table+" SET "+fields[0]+" = \""+values[0]+"\""
+    for i in range(1, len(values)):
+        sql = sql + ", "+fields[i]+" = \""+values[i]+"\""
+    if len(conditions) > 0:
+        sql = sql + " WHERE "+conditions
+    cursor.execute(sql)
+    DB.commit()
+    return True
+
 def run():
     file = open("../Additional_files/evolutions.txt")
     lines = file.read().split("\n")
@@ -71,5 +87,18 @@ def run():
             post_id = post[0][0]
         insert("evolutions", ("pokemon", "level", "evolution"), (pre_id, evo[1], post_id))
 
-run()
+def disable_evolutions(limit):
+    pokes = select("evolutions", ("id", "evolution"), "")
+    ok = {}
+    for poke in pokes:
+        number = int(poke[0][:3])
+        if number <= limit:
+            ok[poke[0]] = "1"
+        else:
+            ok[poke[0]] = "0"
+    keys = ok.keys()
+    for key in keys:
+        update("evolutions", ("avalible",), (ok[key],), "id = "+str(poke[0]))
+
+disable_evolutions(151)
         
