62422c59aeec084f187278f255650d7ef69112d8
Genocider7 <62313980+Genocider7@users.noreply.github.com>
2021-06-02 00:33:49 +0200

    Update get_movelists.py

--- python_scripts/get_movelists.py
+++ python_scripts/get_movelists.py
@@ -268,7 +268,7 @@ def move_to_database(pokemon, move):
 def main():
 #    pokes = select("pokemon", ("national_number", "name"), "convert(substring(national_number, 1, 3), unsigned integer) <= 151 or national_number in (select evolution from evolutions where convert(substring(pokemon, 1, 3), unsigned integer) <= 151)")
 #    pokes = select("pokemon", ("national_number", "name"), "")
-    pokes = select("pokemon", ("national_number", "name"), "")
+    pokes = select("pokemon", ("national_number", "name"), "national_number = 678")
     for poke in pokes:
         time.sleep(0.10)
         print(poke[1]+"...")
