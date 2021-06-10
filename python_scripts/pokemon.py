9a6f96261c1f3830e413b03691e946ecd8a74061
Genocider <blazerwarrior@gmail.com>
2021-06-10 18:50:03 +0100

    Update

--- python_scripts/pokemon.py
+++ python_scripts/pokemon.py
@@ -28,7 +28,7 @@ class pokemon:
 
     def get_data_from_db(self):
         if self.national_number == None:
-            return
+            raise Exception('lol')
         DB = mysql.connector.connect(
             host = 'localhost',
             user = 'psyduck',
@@ -38,7 +38,7 @@ class pokemon:
         cursor = DB.cursor()
         temp = cursor.execute("SELECT regional_number, name, type1, type2, region, emote, shiny_emote, hp, attack, defense, special_attack, special_defense, speed FROM pokemon WHERE national_number = \""+self.national_number+"\"")
         if not temp:
-            return
+            raise Exception('trolol')
         self.regional_number = temp[0]
         self.name = temp[1]
         self.type1 = temp[2]
